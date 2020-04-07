49e93db6595f3e0187b0f343d0c88539cbffca16
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-07 03:38:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,17 @@
 class Owner
-  attr_reader :name
+  attr_reader :name :species
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @species = "human"
+    @@all << self
   end
+
+  def say_species
+    self.species
+  end
+
 
 end
