bb80f637c1a56cb91377722f246468667bd4e7ed
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-07 03:49:51 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,10 +2,16 @@ class Cat
   attr_reader :name
   attr_accessor :owner, :mood
 
+  @@all = []
+
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+  end
+
+  def self.all
+    @@all
   end
 
 
